b2feba09d160d0f7bee739311614d764ecd40ebc
LevinHarris <36119127+levinharris@users.noreply.github.com>
2018-09-04 02:05:57 +0800

    renew

--- JStask11/ver3.js
+++ JStask11/ver3.js
@@ -2,6 +2,7 @@ var probox = document.querySelectorAll(".product")
 var proall = document.querySelector("#proall")
 var regbox = document.querySelectorAll(".region")
 var regall = document.querySelector("#regall")
+var table = document.querySelector("table")
 var proval = new Array()
 var regval = new Array()
 
@@ -10,97 +11,76 @@ function checkselect(){
     for(k=0;k<probox.length;k++){
         probox[k].addEventListener("change",function(){
             proval = []
-            var h = 0
-            for(i=0;i<probox.length;i++){
-                if(probox[i].checked){
-                    h = h + 1
-                    if(h==3){
+            for(ki=0;ki<probox.length;ki++){
+                if (probox[ki].checked) {
+                    proval.push(probox[ki].value)
+                    if(proval.length==3){
                         proall.checked = true
                     }
-                    else if(h<3){
+                    else if(proval.length<3){
                         proall.checked = false
                     }
-                    proval.push(probox[i].value)
                 }
             }
             clearTableData()
-            checktd()
-            // var table = document.querySelector("table")
-            // var sn = 0
-            // for (ss = 0; ss < regbox.length; ss++) {
-            //     if (regbox[ss].checked) {
-            //         sn = sn + 1
-            //     }
-            // }
-            // if (h == 1) { 
-            //     if (sn == 0) {
-            //         table.children[1].children[0].setAttribute("rowspan", "3")
-            //         table.children[2].removeChild(table.children[2].children[0])
-            //         table.children[3].removeChild(table.children[3].children[0])
-            //     }
-            //     // if (sn = 2) {
-            //     //     table.children[1].children[0].setAttribute("rowspan", "2")
-            //     //     table.children[2].removeChild(table.children[2].children[0])
-            //     // }
-            // }
+            checkprotd()
         })
     }
 
     proall.addEventListener("change",function(){
         if(proall.checked){
             proval = []
-            for(i=0;i<probox.length;i++){
-                probox[i].checked = true
-                proval.push(probox[i].value)
+            for(pi=0;pi<probox.length;pi++){
+                probox[pi].checked = true
+                proval.push(probox[pi].value)
             }
         }
         else{
-            for(i=0;i<probox.length;i++){
-                probox[i].checked = false
+            for(ip=0;ip<probox.length;ip++){
+                probox[ip].checked = false
                 proval = []
             }
         }
         clearTableData()
-        checktd()
+        checkprotd()
     })
 
     for(m=0;m<regbox.length;m++){
         regbox[m].addEventListener("change",function(){
             regval = []
-            var u=0
-            for(i=0;i<regbox.length;i++){
-                if(regbox[i].checked){
-                    u=u+1
-                    if(u==3){
+            for(im=0;im<regbox.length;im++){
+                if (regbox[im].checked) {
+                    regval.push(regbox[im].value)
+                    if(regval.length==3){
                         regall.checked = true
                     }
-                    else if(u<3){
+                    else if(regval.length<3){
                         regall.checked = false
                     }
-                    regval.push(regbox[i].value)
+                    
                 }
             }
             clearTableData()
-            checktd()
+            checkregtd()
         })
     }
 
     regall.addEventListener("change",function(){
         if(regall.checked){
             regval = []
-            for(i=0;i<regbox.length;i++){
-                regbox[i].checked = true
-                regval.push(regbox[i].value)
+            for(ri=0;ri<regbox.length;ri++){
+                regbox[ri].checked = true
+                regval.push(regbox[ri].value)
             }
         }
         else{
-            for(i=0;i<regbox.length;i++){
-                regbox[i].checked = false
+            for(ir=0;ir<regbox.length;ir++){
+                regbox[ir].checked = false
                 regval = []
             }
         }
         clearTableData()
-        checktd()
+        checkregtd()
     })
 }
 
@@ -139,100 +119,205 @@ function getcheckData(){
     return selectedData
 }
 
-function checktd(){
+function checkprotd() {
     var tempdata = getcheckData()
     console.log(tempdata)
-    for(i=0;i<tempdata.length;i++){
-        var tr = document.createElement("tr")
-        var th = document.querySelector("table").firstElementChild.childNodes
-        for(j=0;j<th.length;j++){
-            var td = document.createElement("td")
-            tr.appendChild(td)
+    var tempnum = 0
+    var regnum = 0
+    for (sr = 0; sr < regbox.length; sr++) {
+        if (regbox[sr].checked) {
+            regnum++
         }
-        var table = document.querySelector("table")
-        table.appendChild(tr)
     }
-    var trtemp = document.querySelectorAll("tr")
+    for (m = 0; m < proval.length; m++){
+        for (i = 0; i < tempdata.length; i++) {
+            var temppro = document.createTextNode(tempdata[i].product)
+            var tempreg = document.createTextNode(tempdata[i].region)
+            var th = table.firstElementChild.childNodes
+            if (proval[m] == tempdata[i].product) {
+                if (tempnum == m) {
+                    var tr = document.createElement("tr")
+                    for (j = 0; j < th.length; j++) {
+                        var td = document.createElement("td")
+                        tr.appendChild(td)
+                    }
+                    var tdtemp = tr.querySelectorAll("td")
+                    var td0 = tdtemp[0]
+                    var td1 = tdtemp[1]
+                    td0.appendChild(temppro)
+                    td1.appendChild(tempreg)
+                    for (l = 2; l < 14; l++) {
+                        var s = l - 2
+                        var tempsale = document.createTextNode(tempdata[i].sale[s])
+                        tdtemp[l].appendChild(tempsale)
+                    }
+                    if (regnum == 0 || regnum > 2) {
+                        if (i == 0) {
+                            td0.setAttribute("rowspan", probox.length)
+                        }
+                        else {
+                            tr.removeChild(tr.children[0])
+                        }
+                    }
+                    else {
+                        if (i == 0) {
+                            td0.setAttribute("rowspan", regnum)
+                        }
+                        else {
+                            tr.removeChild(tr.children[0])
+                        }
+                    }
+                    table.appendChild(tr)
+                }
+                else { 
+                    var tr = document.createElement("tr")
+                    for (j = 0; j < th.length; j++) {
+                        var td = document.createElement("td")
+                        tr.appendChild(td)
+                    }
+                    var tdtemp = tr.querySelectorAll("td")
+                    var td0 = tdtemp[0]
+                    var td1 = tdtemp[1]
+                    td0.appendChild(temppro)
+                    td1.appendChild(tempreg)
+                    for (l = 2; l < 14; l++) {
+                        var s = l - 2
+                        var tempsale = document.createTextNode(tempdata[i].sale[s])
+                        tdtemp[l].appendChild(tempsale)
+                    }
+                    if (regnum == 0 || regnum > 2) {
+                        td0.setAttribute("rowspan", probox.length)
+                    }
+                    else { 
+                        td0.setAttribute("rowspan", regnum)
+                    }
+                    table.appendChild(tr)
+                    tempnum = m
+                }   
+            }
+        }    
+    }   
+}
+
+
+function checkregtd() {
+    var tempdata = getcheckData()
+    console.log(tempdata)
+    var tempnum = 0
     var pronum = 0
     var regnum = 0
-    for (s1 = 0; s1 < probox.length; s1++){
-        if (probox[s1].checked) {
-            pronum = pronum +1
+    for (sp = 0; sp < probox.length; sp++){
+        if (probox[sp].checked) {
+            pronum++
         }
     }
-    for (s2 = 0; s2 < probox.length; s2++){
-        if (regbox[s2].checked) {
-            regnum = regnum +1
+    for (sr = 0; sr < regbox.length; sr++) {
+        if (regbox[sr].checked) {
+            regnum++
         }
     }
-    for(k=0;k<tempdata.length;k++){
-        var temppro = document.createTextNode(tempdata[k].product)
-        var tempreg = document.createTextNode(tempdata[k].region)
-        var tdtemp = trtemp[k+1].querySelectorAll("td")
-        var td0 = tdtemp[0]
-        var td1 = tdtemp[1]
-        td0.appendChild(temppro)
-        td1.appendChild(tempreg)
-        for(l=2;l<14;l++){
-            var s = l-2
-            var tempsale = document.createTextNode(tempdata[k].sale[s])
-            tdtemp[l].appendChild(tempsale)
+    if (pronum>1) {
+        var ths = table.querySelectorAll("th")
+        var ths1 = ths[1].innerText
+        if (ths1 == "地区") {
+            ths[1].innerText = ths[0].innerText
+            ths[0].innerText = ths1
         }
     }
-
-    // 这里需求暂时用了土办法,先交作业.回头再修改
-    if (pronum == 1) {
-        if (regnum == 0) {
-            table.children[1].children[0].setAttribute("rowspan", "3")
-            table.children[2].removeChild(table.children[2].children[0])
-            table.children[3].removeChild(table.children[3].children[0])
-        }
-        if (regnum == 2) { 
-            table.children[1].children[0].setAttribute("rowspan", "2")
-            table.children[2].removeChild(table.children[2].children[0])
-        }
-        if (regnum == 3) { 
-            table.children[1].children[0].setAttribute("rowspan", "3")
-            table.children[2].removeChild(table.children[2].children[0])
-            table.children[3].removeChild(table.children[3].children[0])
-        }
-    }
-    if (regnum == 1) {
-        table.children[0].children[0].innerHTML = "地区"
-        table.children[0].children[1].innerHTML = "商品"
-        if (pronum == 0) {
-            var tempregtxt = table.children[1].children[1].innerHTML
-            var temprotxt = table.children[1].children[0].innerHTML
-            table.children[1].children[0].innerHTML = tempregtxt
-            table.children[1].children[1].innerHTML = temprotxt
-            table.children[2].children[1].innerHTML = table.children[2].children[0].innerHTML
-            table.children[3].children[1].innerHTML = table.children[3].children[0].innerHTML
-            table.children[1].children[0].setAttribute("rowspan", "3")
-            table.children[2].removeChild(table.children[2].children[0])
-            table.children[3].removeChild(table.children[3].children[0])
-        }
-        if (pronum == 2) { 
-            var tempregtxt = table.children[1].children[1].innerHTML
-            var temprotxt = table.children[1].children[0].innerHTML
-            table.children[1].children[0].innerHTML = tempregtxt
-            table.children[1].children[1].innerHTML = temprotxt
-            table.children[2].children[1].innerHTML = table.children[2].children[0].innerHTML
-            table.children[1].children[0].setAttribute("rowspan", "2")
-            table.children[2].removeChild(table.children[2].children[0])
-        }
-        if (pronum == 3) { 
-            var tempregtxt = table.children[1].children[1].innerHTML
-            var temprotxt = table.children[1].children[0].innerHTML
-            table.children[1].children[0].innerHTML = tempregtxt
-            table.children[1].children[1].innerHTML = temprotxt
-            table.children[2].children[1].innerHTML = table.children[2].children[0].innerHTML
-            table.children[3].children[1].innerHTML = table.children[3].children[0].innerHTML
-            table.children[1].children[0].setAttribute("rowspan", "3")
-            table.children[2].removeChild(table.children[2].children[0])
-            table.children[3].removeChild(table.children[3].children[0])
-        }
-    }
+    for (m = 0; m < regval.length; m++){
+        for (i = 0; i < tempdata.length; i++) {
+            var temppro = document.createTextNode(tempdata[i].product)
+            var tempreg = document.createTextNode(tempdata[i].region)
+            var th = table.firstElementChild.childNodes
+            if (regval[m] == tempdata[i].region) {
+                if (tempnum == m) {
+                    var tr = document.createElement("tr")
+                    for (j = 0; j < th.length; j++) {
+                        var td = document.createElement("td")
+                        tr.appendChild(td)
+                    }
+                    var tdtemp = tr.querySelectorAll("td")
+                    var td0 = tdtemp[0]
+                    var td1 = tdtemp[1]
+                    td1.appendChild(temppro)
+                    td0.appendChild(tempreg)
+                    for (l = 2; l < 14; l++) {
+                        var s = l - 2
+                        var tempsale = document.createTextNode(tempdata[i].sale[s])
+                        tdtemp[l].appendChild(tempsale)
+                    }
+                    if (pronum == 1) {
+                        checkprotd()
+                        return
+                    }
+                    else if (pronum == 0 || pronum > 2) {
+                        if (i == 0) {
+                            td0.setAttribute("rowspan", regbox.length)
+                        }
+                        else {
+                            tr.removeChild(tr.children[0])
+                        }
+                    }
+                    else {
+                        if (regnum == 1) {
+                            if (i == 0) {
+                                td0.setAttribute("rowspan", pronum)
+                            }
+                            else {
+                                tr.removeChild(tr.children[0])
+                            }
+                        }
+                        else { 
+                            if (m == i) {
+                                if (i == 0) {
+                                    td0.setAttribute("rowspan", pronum)
+                                }
+                                else {
+                                    tr.removeChild(tr.children[0])
+                                }
+                            }
+                            else { 
+                                if (i == 1 && m == 0) {
+                                    td0.setAttribute("rowspan", pronum)
+                                }
+                                else {
+                                    tr.removeChild(tr.children[0])
+                                }
+                            }
+                        }
+                    }                        
+                    table.appendChild(tr)
+                }
+                else { 
+                    var tr = document.createElement("tr")
+                    for (j = 0; j < th.length; j++) {
+                        var td = document.createElement("td")
+                        tr.appendChild(td)
+                    }
+                    var tdtemp = tr.querySelectorAll("td")
+                    var td0 = tdtemp[0]
+                    var td1 = tdtemp[1]
+                    td1.appendChild(temppro)
+                    td0.appendChild(tempreg)
+                    for (l = 2; l < 14; l++) {
+                        var s = l - 2
+                        var tempsale = document.createTextNode(tempdata[i].sale[s])
+                        tdtemp[l].appendChild(tempsale)
+                    }
+                    if (pronum == 0 || pronum > 2) {
+                        td0.setAttribute("rowspan", regbox.length)
+                    }
+                    else { 
+                        td0.setAttribute("rowspan", pronum)
+                    }
+                    table.appendChild(tr)
+                    tempnum = m
+                }   
+            }
+        }    
+    } 
 }
+
 
 
 
